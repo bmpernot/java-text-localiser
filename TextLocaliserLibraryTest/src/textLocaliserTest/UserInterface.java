744042dca317f3f2cc1eaa803021383a05f006b9
bmpernot <61884694+bmpernot@users.noreply.github.com>
2020-03-30 11:15:01 +0100

    tring to apply code

--- TextLocaliserLibraryTest/src/textLocaliserTest/UserInterface.java
+++ TextLocaliserLibraryTest/src/textLocaliserTest/UserInterface.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class UserInterface {
 
-	public static void main (String [] args) {
+public static void main (String [] args) {
 		
 		Scanner userInput = new Scanner(System.in);
 		
